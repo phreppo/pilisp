c766ea11c8b965c79d9ebe6fbae8892c3c4ecf44
phreppo <parolini.francesco.1@gmail.com>
2018-05-23 13:04:58 +0200

    come proofs about genric pointers

--- include/pilisp.h
+++ include/pilisp.h
@@ -12,11 +12,32 @@
 #include <stdio.h>
 
 /**
+ * 
  * @brief Returns zero, nothing more
  * 
  * @return int zero value
  */
 int zero();
+
+/**
+ * @brief general compare
+ * 
+ * @param i first number
+ * @param j second number
+ * @return int always 0
+ */
+int general_compare(int i,int j);
+
+/**
+ * @brief sort alogorithm for generical array
+ * 
+ * @param base base address
+ * @param nmemb number of elements
+ * @param size size of the elements
+ * @param compar compare function
+ */
+void qsort(void *base[], size_t nmemb, size_t size, 
+           int (*compar)(const void *, const void *));
 
 /**
  * @brief Square root calculator, using math.h function
--- src/pilisp.c
+++ src/pilisp.c
@@ -1,21 +1,40 @@
 #include "pilisp.h"
 
-int zero(){
+int general_compare(int i, int j)
+{
     return 0;
 }
 
-double radiceQuadrata(double d){
-    if(0){
+void qsort(void *base[], size_t nmemb, size_t size,
+           int (*compar)(const void *, const void *))
+{
+    (*compar)(base[0],base[1]);
+    return;
+}
+
+int zero()
+{
+    return 0;
+}
+
+double radiceQuadrata(double d)
+{
+    if (0)
+    {
         return 0;
     }
-    if (d<0){
+    if (d < 0)
+    {
         printf("we");
-    } else {
+    }
+    else
+    {
         printf("zio");
     }
     return sqrt(d);
 }
 
-void stampaQualcosa(){
+void stampaQualcosa()
+{
     printf("ciaux\n");
 }
