13ac37bbf54737dd1f7ea98fbc6102cc1524a632
phreppo <parolini.francesco.1@gmail.com>
2018-05-23 15:02:34 +0200

    indentation

--- include/pilisp.h
+++ include/pilisp.h
@@ -12,46 +12,46 @@
 #include <stdio.h>
 
 /**
- * 
+ *
  * @brief Returns zero, nothing more
- * 
+ *
  * @return int zero value
  */
 int zero();
 
 /**
  * @brief general compare
- * 
+ *
  * @param i first number
  * @param j second number
  * @return int always 0
  */
-int general_compare(int i,int j);
+int general_compare(int i, int j);
 
 /**
  * @brief sort alogorithm for generical array
- * 
+ *
  * @param base base address
  * @param nmemb number of elements
  * @param size size of the elements
  * @param compar compare function
  */
-void qsort(void *base[], size_t nmemb, size_t size, 
+void qsort(void *base[], size_t nmemb, size_t size,
            int (*compar)(const void *, const void *));
 
 /**
  * @brief Square root calculator, using math.h function
- * 
- * @param d 
- * @return double 
+ *
+ * @param d
+ * @return double
  */
 double radiceQuadrata(double d);
 
 /**
  * @brief Stampa delle cose relativamente utili
- * 
+ *
  * Non è molto utile effettivamente
- * 
+ *
  */
 void stampaQualcosa();
 
--- src/pilisp.c
+++ src/pilisp.c
@@ -1,40 +1,25 @@
 #include "pilisp.h"
 
-int general_compare(int i, int j)
-{
-    return 0;
-}
+int general_compare(int i, int j) { return 0; }
 
 void qsort(void *base[], size_t nmemb, size_t size,
-           int (*compar)(const void *, const void *))
-{
-    (*compar)(base[0],base[1]);
-    return;
+           int (*compar)(const void *, const void *)) {
+  (*compar)(base[0], base[1]);
+  return;
 }
 
-int zero()
-{
+int zero() { return 0; }
+
+double radiceQuadrata(double d) {
+  if (0) {
     return 0;
+  }
+  if (d < 0) {
+    printf("we");
+  } else {
+    printf("zio");
+  }
+  return sqrt(d);
 }
 
-double radiceQuadrata(double d)
-{
-    if (0)
-    {
-        return 0;
-    }
-    if (d < 0)
-    {
-        printf("we");
-    }
-    else
-    {
-        printf("zio");
-    }
-    return sqrt(d);
-}
-
-void stampaQualcosa()
-{
-    printf("ciaux\n");
-}
+void stampaQualcosa() { printf("ciaux\n"); }
