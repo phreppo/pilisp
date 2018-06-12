166c22f25bc363204cbe8270a44429f426125ca8
phreppo <parolini.francesco.1@gmail.com>
2018-06-12 12:27:56 +0200

    docs

--- test/parser_test.c
+++ test/parser_test.c
@@ -2,7 +2,7 @@
 #include "pitestutils.h"
 #include <stdio.h>
 
-int parser_test(char * file_path_name) {
+int parser_test(char *file_path_name) {
   FILE *fp;
   if ((fp = fopen(file_path_name, "r")) == NULL) {
     printf("Error opening file\n");
@@ -13,9 +13,9 @@ int parser_test(char * file_path_name) {
   return 0;
 }
 
-int main(int argc, char **argv) { 
-    char * file_path_name = argv[1];
-    printf("File name: %s\n",file_path_name);
-    parser_test(file_path_name);
-    return 0; 
+int main(int argc, char **argv) {
+  char *file_path_name = argv[1];
+  printf("File name: %s\n", file_path_name);
+  parser_test(file_path_name);
+  return 0;
 }
