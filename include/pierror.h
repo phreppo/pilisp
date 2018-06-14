0b1ac287541084bcacc5ab0f35c2008862f5942c
phreppo <parolini.francesco.1@gmail.com>
2018-06-14 17:36:39 +0200

    pierror documentation

--- include/pierror.h
+++ include/pierror.h
@@ -13,21 +13,50 @@
 #include <string.h>
 #include "pilisp.h"
 
+/**
+ * @brief enum to idenfity the type of the last erro
+ * 
+ */
 enum{
-    NO_ERROR = -1,
-    LISP_ERROR = 1,
-    MEMORY_ERROR = 2,
-    MODE_ERROR = 3
+    NO_ERROR = -1,           // no error occurrred
+    LISP_ERROR = 1,          // LISP syntax error
+    MEMORY_ERROR = 2,        // memory error
+    MODE_ERROR = 3           // error passing mode to some kind of functions
 };
 
+/**
+ * @brief last error occurred
+ * 
+ */
 static int last_error=NO_ERROR;
 
+/**
+ * @brief sets the last error to CODE, and sets one message. It will longjump to the last saved point
+ * 
+ * @param CODE code of the error
+ * @param message message to display
+ */
 void pi_error(int CODE, const char* message);
 
+/**
+ * @brief Get the last error code
+ * 
+ * @return int last error code
+ */
 int get_last_error();
 
+/**
+ * @brief one error just verificated?
+ * 
+ * @return true yes
+ * @return false no
+ */
 bool had_error();
 
+/**
+ * @brief after reading one error this function should be called to indicate that the error has benn handled
+ * 
+ */
 void reset_error();
 
 #endif // !PERROR_H
