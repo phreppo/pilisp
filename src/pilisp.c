25eb5fa41cbeb3bb967f30fa04b688b38c055459
phreppo <parolini.francesco.1@gmail.com>
2018-07-19 09:07:20 +0200

    logo

--- src/pilisp.c
+++ src/pilisp.c
@@ -6,17 +6,35 @@ int pi_prompt() {
   // __)(  _ \\\n  )___/ _)(_  )(__  _)(_ \\__ \\ )___/\n (__)
   // (____)(____)(____)(___/(__) \n\n");
 
-  printf(COLOR1 "\n\t0000000000000000000000000000000\n\t00" COLOR2
-                "         _ _ _             " COLOR1 "00\n\t00" COLOR2
-                "        (_) (_)            " COLOR1 "00\n\t00" COLOR2
-                "   _ __  _| |_ ___ _ __    " COLOR1 "00\n\t00" COLOR2
-                "  | '_ \\| | | / __| '_ \\   " COLOR1 "00\n\t00" COLOR2
-                "  | |_) | | | \\__ \\ |_) |  " COLOR1 "00\n\t00" COLOR2
-                "  | .__/|_|_|_|___/ .__/   " COLOR1 "00\n\t00" COLOR2
-                "  | |             | |      " COLOR1 "00\n\t00" COLOR2
-                "  |_|             |_|      " COLOR1 "00\n\t00" COLOR2
-                "                           " COLOR1
-                "00\n\t0000000000000000000000000000000 \n\n" ANSI_COLOR_RESET);
+  // printf(COLOR1 "\n\t0000000000000000000000000000000\n\t00" COLOR2
+  //               "         _ _ _             " COLOR1 "00\n\t00" COLOR2
+  //               "        (_) (_)            " COLOR1 "00\n\t00" COLOR2
+  //               "   _ __  _| |_ ___ _ __    " COLOR1 "00\n\t00" COLOR2
+  //               "  | '_ \\| | | / __| '_ \\   " COLOR1 "00\n\t00" COLOR2
+  //               "  | |_) | | | \\__ \\ |_) |  " COLOR1 "00\n\t00" COLOR2
+  //               "  | .__/|_|_|_|___/ .__/   " COLOR1 "00\n\t00" COLOR2
+  //               "  | |             | |      " COLOR1 "00\n\t00" COLOR2
+  //               "  |_|             |_|      " COLOR1 "00\n\t00" COLOR2
+  //               "                           " COLOR1
+  //               "00\n\t0000000000000000000000000000000 \n\n" ANSI_COLOR_RESET);
+
+  printf(
+      COLOR1
+      "\n"
+      "===============================================================\n" COLOR2
+      COLOR1 "" COLOR2"\t             88  88  88                          \n" 
+      COLOR1 "" COLOR2"\t             \"\"  88  \"\"                          \n" 
+      COLOR1 "" COLOR2"\t                 88                              \n" 
+      COLOR1 "" COLOR2"\t8b,dPPYba,   88  88  88  ,adPPYba,  8b,dPPYba,   \n" 
+      COLOR1 "" COLOR2"\t88P'    \"8a  88  88  88  I8[    \"\"  88P'    \"8a  \n" 
+      COLOR1 "" COLOR2"\t88       d8  88  88  88   `\"Y8ba,   88       d8  \n" 
+      COLOR1 "" COLOR2"\t88b,   ,a8\"  88  88  88  aa    ]8I  88b,   ,a8\"  \n" 
+      COLOR1 "" COLOR2"\t88`YbbdP\"'   88  88  88  `\"YbbdP\"'  88`YbbdP\"'   \n" 
+      COLOR1 "" COLOR2"\t88                                  88           \n" 
+      COLOR1 "" COLOR2"\t88                                  88           \n" COLOR1 
+      "===============================================================\n\n"
+
+      ANSI_COLOR_RESET);
 
   bool repeat = true;
   while (repeat) {
@@ -37,7 +55,7 @@ int pi_prompt() {
     printf(ANSI_COLOR_GREEN ":) " ANSI_COLOR_RESET);
     print_sexpr(result);
     puts("");
-    if (result == symbol_bye) 
+    if (result == symbol_bye)
       repeat = false;
     else
       cell_remove_recursive(result);
